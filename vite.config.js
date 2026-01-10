9bafbaa067f9c645b0552577a4e6594fa0b7bbfb
thithiramalsen <thithiraop1@gmail.com>
2026-01-11 01:29:06 +0530

    portfolio running

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcsss from "tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite"
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcsss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
